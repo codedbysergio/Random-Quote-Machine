0de3316e6de46611af1769267b56a94aa4ad6b37
Sergio Garcia <61205864+codedbysergio@users.noreply.github.com>
2023-01-23 14:33:47 -0600

    removed useless string concatenation

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Twitter({ quotes, number}) {
 
   function handleClickTwitter() {
-    const twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22"
-    const quoteToTweet = quotes[number][1] + "%22" + " -" + quotes[number][0];
-    const tweetQuote = twitterLink + quoteToTweet
+    const tweetQuote = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22" + quotes[number][1] + "%22" + " -" + quotes[number][0];
     window.open(tweetQuote, '_blank')
   }
   return (
