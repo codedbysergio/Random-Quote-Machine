09a61d4926dc1479dcbaef7af874126a32709b49
Sergio Garcia <61205864+codedbysergio@users.noreply.github.com>
2023-01-23 14:56:39 -0600

    fixed facebook link

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -6,7 +6,7 @@ export default function Facebook() {
       className="button" 
       id="post-quote" 
       title="Share Page on Facebook!"
-      href="https://www.facebook.com/sharer/sharer.php?u=codepen.io/codedbysergio/pen/jOZjxpW"
+      href="https://www.facebook.com/sharer/sharer.php?u=https://main--candid-semifreddo-ed0039.netlify.app/"
     >
         <i 
           className="fa-brands fa-facebook-f"
